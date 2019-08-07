b3b70c5ffd7f6e9f17a1490faaa9921dff295eb3
Ranispoorti <43165149+Ranispoorti@users.noreply.github.com>
2019-08-07 13:20:20 +0530

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+#include<iostream>
+using namespace std;
+ 
+
+
+void find(int N,int k,int a[])
+{
+  int f=0;
+  int l=N-1;
+  int m;
+  
+  while(f<=l)
+   { m=(l+f)/2;
+     if(k == a[m])
+      { cout<<"1";
+         break;}
+     else if(k<a[m])
+      { l=m-1;}
+     else
+      {f=m+1;}
+   }
+   if(f>l)
+   { cout<<"-1";}
+}
+
+int  main()
+{
+  int t,k,N,a[100];
+  cin>>t;
+  for(int i=0;i<t;i++)
+  {
+    cin>>N>>k;
+    for(int j=0;j<N;j++)
+     {
+       cin>>a[j];
+     }
+     find(N,k,a);
+   } 
+ return 0;
+}
+ 
+
+
+
