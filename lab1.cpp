06cd4b7fd8a7a85e12d0c4af0f1205d400467ea4
Ranispoorti <43165149+Ranispoorti@users.noreply.github.com>
2019-08-07 11:54:43 +0530

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+Class search
+{
+
+void main()
+{
+  int t,k,N,a[N];
+  cin>>t;
+  for(int i=0;i<t;i++)
+  {
+    cin>>N>>" ">>k;
+    for(int j=0;j<N;j++)
+     {
+       cin>>a[j];
+     }
+   } 
+ 
+}
+ 
+void find(arr[N],N,k)
+{
+  int f=0,l=N-1,m;
+  
+}
