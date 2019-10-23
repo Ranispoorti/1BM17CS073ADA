bde2e0356f0110ddaa0c87c787d007534a04f7ec
Ranispoorti <43165149+Ranispoorti@users.noreply.github.com>
2019-10-23 13:00:44 +0530

    Add files via upload

---
+++ week8 1.py
@@ -0,0 +1,14 @@
+def isHeap(arr, n): 
+    for i in range(int((n - 2) / 2) + 1): 
+        if arr[2 * i + 1] > arr[i]:  
+                return False
+        if (2 * i + 2 < n and
+            arr[2 * i + 2] > arr[i]):  
+                return False
+    return True
+arr = [90, 15, 10, 7, 12, 2, 7, 3]  
+n = len(arr) 
+if isHeap(arr, n): 
+    print("Yes") 
+else: 
+    print("No") 
